ed97640b4e7e6a06f9effb01a546ce2ac4d047e7
Vernie Namca <vernie@mybusybee.net>
2021-03-25 11:03:46 +0800

    Created an EF migration for benefit status table
    Created an EF migration for benefit status table.

--- src/Infrastructure/Migrations/20210325025644_add-benefit-status-table.Designer.cs
+++ src/Infrastructure/Migrations/20210325025644_add-benefit-status-table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PVAO.Infrastructure.Data;
 
 namespace PVAO.Infrastructure.Migrations
 {
     [DbContext(typeof(PVAOContext))]
-    partial class PVAOContextModelSnapshot : ModelSnapshot
+    [Migration("20210325025644_add-benefit-status-table")]
+    partial class addbenefitstatustable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Migrations/20210325025644_add-benefit-status-table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PVAO.Infrastructure.Migrations
+{
+    public partial class addbenefitstatustable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BenefitStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(maxLength: 250, nullable: false),
+                    Prefix = table.Column<string>(maxLength: 15, nullable: false),
+                    Claimant = table.Column<string>(maxLength: 150, nullable: true),
+                    CreatedBy = table.Column<int>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<int>(nullable: true),
+                    DateUpdated = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BenefitStatus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BenefitStatus");
+        }
+    }
+}
