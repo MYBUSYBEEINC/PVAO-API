5ec72f7ac98a8c94e6cff7913bd63996d9adb55b
Cave, Don (TaguigCity1634) <don.cave@towerswatson.com>
2021-05-22 17:16:32 +0800

    Added GetTotalOverremittancesCount in the Dashboard

--- src/API/Controllers/DashboardController.cs
+++ src/API/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using PVAO.ApplicationCore.Entities.Structure;
@@ -12,11 +13,13 @@ namespace PVAO.API.Controllers
     {
         private readonly IVeteranService _veteranService;
         private readonly IBeneficiaryService _beneficiaryService;
+        private readonly IClaimApplicationService _claimApplicationService;
 
-        public DashboardController(IVeteranService veteranService, IBeneficiaryService beneficiaryService)
+        public DashboardController(IVeteranService veteranService, IBeneficiaryService beneficiaryService, IClaimApplicationService claimApplicationService)
         {
             _veteranService = veteranService;
             _beneficiaryService = beneficiaryService;
+            _claimApplicationService = claimApplicationService;
         }
 
         [HttpGet("[action]")]
@@ -35,6 +38,22 @@ namespace PVAO.API.Controllers
         public int GetTotalBeneficiaries()
         {
             return _beneficiaryService.Get().Count();
+        }
+
+        [HttpGet("[action]")]
+        public int GetTotalOverremittances()
+        {
+            IQueryable<ClaimApplication> claimApplications = _claimApplicationService.Get();
+            List<Veteran> veterans = _veteranService.Get().ToList();
+            int totalCount = 0;
+            foreach (ClaimApplication item in claimApplications.ToList())
+            {
+                var veteran = veterans.FirstOrDefault(x => x.vdmsno.ToString() == item.vdmsNo);
+                if (veteran == null) throw new ApplicationException($"Veteran with Id {item.vdmsNo} not found!");
+                if (veteran.mortalStatus == "DECEASED") totalCount++;
+            }
+
+            return totalCount;
         }
     }
 }
